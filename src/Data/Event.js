0628fdfa7205322926635b1fd388ab566c913d46
Ankitgsoc24 <ankit27092004@gmail.com>
2024-01-30 23:53:38 +0530

    Add padding to whole container in facility section

--- src/Data/Event.js
+++ src/Data/Event.js
@@ -8,48 +8,48 @@ import event7 from "../Assets/Events Picture/7.png";
 
 
 const eventData = [
-{
-    image: event1,
-    title: '75th INDEPENDENCE DAY',
-    description: 'Reflecting on the Splendor of Our 75th Independence Day Celebration! Gratitude fills our hearts as we look back on an unforgettable event that brought our community together in the spirit of patriotism'
+    {
+        image: event1,
+        title: '75th INDEPENDENCE DAY',
+        description: 'Reflecting on the Splendor of Our 75th Independence Day Celebration! Gratitude fills our hearts as we look back on an unforgettable event that brought our community together in the spirit of patriotism'
 
-},
-{
-    image: event2,
-    title: 'Inauguration of Badminton Court',
-    description: 'Celebrating the Grand Inauguration of Our New Badminton Court! The excitement and energy of the event still linger in the air as we reflect on a fantastic day filled with sportsmanship and camaraderie'
+    },
+    {
+        image: event2,
+        title: 'Inauguration of Badminton Court',
+        description: 'Celebrating the Grand Inauguration of Our New Badminton Court! The excitement and energy of the event still linger in the air as we reflect on a fantastic day filled with sportsmanship and camaraderie'
 
-},
-{
-    image: event3,
-    title: 'Inaugration of Eco Club',
-    description: 'Reflecting on the Inauguration of Our Eco Club! It was a day of green beginnings and sustainable aspirations as we officially launched our Eco Club. The event brought together passionate individuals committed to environmental stewardship'
+    },
+    {
+        image: event3,
+        title: 'Inauguration of Eco Club',
+        description: 'Reflecting on the Inauguration of Our Eco Club! It was a day of green beginnings and sustainable aspirations as we officially launched our Eco Club. The event brought together passionate individuals committed to environmental stewardship'
 
-},
-{
-    image: event4,
-    title: 'Inaugration of Rastriya Seva Yojane',
-    description: 'Reflecting on the Grand Inauguration of "Rastriya Seva Yojane"! It was a momentous occasion filled with pride and commitment as we officially launched this noble initiative. The event brought together individuals dedicated to national service, fostering a spirit of unity and collective action'
+    },
+    {
+        image: event4,
+        title: 'Inauguration of Rastriya Seva Yojane',
+        description: 'Reflecting on the Grand Inauguration of "Rastriya Seva Yojane"! It was a momentous occasion filled with pride and commitment as we officially launched this noble initiative. The event brought together individuals dedicated to national service, fostering a spirit of unity and collective action'
 
-},
-{
-    image: event5,
-    title: 'Inaugration of Scouts and Guides',
-    description: 'Celebrating the Inauguration of Scouts and Guides! The event marked a significant milestone as we officially launched this empowering initiative. Together, we embarked on a journey of leadership, camaraderie, and skill development. A heartfelt thank you to everyone who joined us in ushering in this exciting chapter'
+    },
+    {
+        image: event5,
+        title: 'Inauguration of Scouts and Guides',
+        description: 'Celebrating the Inauguration of Scouts and Guides! The event marked a significant milestone as we officially launched this empowering initiative. Together, we embarked on a journey of leadership, camaraderie, and skill development. A heartfelt thank you to everyone who joined us in ushering in this exciting chapter'
 
-},
-{
-    image: event6,
-    title: 'Event to create awareness about environmental health by MLA of yellanka, S. R . Vishwanath',
-    description: "In a concerted effort to prioritize our community's well-being, MLA S. R. Vishwanath orchestrated a compelling event to raise awareness about environmental health in Yellanka. The gathering served as a platform to enlighten residents on sustainable practices, pollution control, and the vital role each individual plays in preserving our environment"
+    },
+    {
+        image: event6,
+        title: 'Event to create awareness about environmental health by MLA of yellanka, S. R . Vishwanath',
+        description: "In a concerted effort to prioritize our community's well-being, MLA S. R. Vishwanath orchestrated a compelling event to raise awareness about environmental health in Yellanka. The gathering served as a platform to enlighten residents on sustainable practices, pollution control, and the vital role each individual plays in preserving our environment"
 
-},
-{
-    image: event7,
-    title: 'Opening ceremony of batmintion Court',
-    description: 'Celebrating the Grand Opening of Our New Badminton Court! The energy was electrifying as we came together to inaugurate our state-of-the-art badminton court. The opening ceremony was a testament to our commitment to promoting a healthy and active lifestyle within our community. '
+    },
+    {
+        image: event7,
+        title: 'Opening ceremony of Badminton Court',
+        description: 'Celebrating the Grand Opening of Our New Badminton Court! The energy was electrifying as we came together to inaugurate our state-of-the-art badminton court. The opening ceremony was a testament to our commitment to promoting a healthy and active lifestyle within our community. '
 
-}
+    }
 ]
 
 export default eventData
