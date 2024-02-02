0413584474ddfd9cd5e78f0218bf930fa1b90c9c
Sarthak Patel <sarthakpatel230204@gmail.com>
2024-02-03 01:42:01 +0530

    Layout Fix Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../Assets/Logo/logo.png";
 import { Link, Outlet } from "react-router-dom";
+
+//Icons for Top Navbar
 import { FaPhone } from "react-icons/fa6";
 import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
-
 
 import { FaCaretDown } from "react-icons/fa";
 
@@ -17,7 +18,6 @@ import { SiGooglescholar } from "react-icons/si";
 //Icons for Admission Submenu
 import { FaMoneyBill } from "react-icons/fa";
 
-
 // Icons for Department
 import { BiSolidSchool } from "react-icons/bi";
 import { FaSchool } from "react-icons/fa6";
@@ -27,8 +27,6 @@ import { FaSchoolFlag } from "react-icons/fa6";
 import { FaImage } from "react-icons/fa";
 import { FaVideo } from "react-icons/fa";
 
-
-
 // Icons for About
 import { FaUsers } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
@@ -36,7 +34,6 @@ import { FaUser } from "react-icons/fa";
 import { RiUserStarFill } from "react-icons/ri";
 
 const Navbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
   const [openSubmenu, setOpenSubmenu] = useState(null);
 
@@ -45,7 +42,9 @@ const Navbar = () => {
   };
 
   const toggleSubmenu = (submenuId) => {
-    setOpenSubmenu((prevSubmenu) => (prevSubmenu === submenuId ? null : submenuId));
+    setOpenSubmenu((prevSubmenu) =>
+      prevSubmenu === submenuId ? null : submenuId
+    );
   };
 
   // Use separate refs for each submenu button
@@ -76,116 +75,227 @@ const Navbar = () => {
     };
   }, []);
 
-
-
   return (
     <>
-    <div className="top-nav">
-      <div className="info-start">
-        <ul>
-            <li> <FaPhone /> +91 9901773645</li>
-        </ul>
-      </div>
+      <div className="top-nav">
+        <div className="info-start">
+          <ul>
+            <li>
+              
+              <FaPhone /> +91 9901773645
+            </li>
+          </ul>
+        </div>
         <div class="info-end">
-        <ul>
-            <li> <Link to="/academic-overview"> Overview </Link> </li>
-            <li> <Link to="/contact"> Help </Link> </li>
-            <li className="cta-button"><Link to='/vision-mission'>Vision & Mission </Link></li>
-
-        </ul>
-    </div>
-    </div>
-    {/* <div class = "info-nav">
-    <div class="info-1">
-        <ul>
-            <li> <FaPhone /> +91 9901773645</li>
-        </ul>
-    </div>
-
-    <div class="info-2">
-        <ul>
-            <li> <Link to="/academic-overview"> Overview </Link> </li>
-            <li> <Link to="/contact"> Help </Link> </li>
-            <li className="cta-button"><Link to='/vision-mission'>Vision & Mission </Link></li>
-
-        </ul>
-    </div>
-    
-    </div> */}
-
+          <ul>
+            <li>
+              
+              <Link to="/academic-overview"> Overview </Link>
+            </li>
+            <li>
+              
+              <Link to="/contact"> Help </Link>
+            </li>
+            <li className="cta-button">
+              <Link to="/vision-mission">Vision & Mission </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <nav id="navbar">
         <div class="nav-container-1">
           <div className="logo-container">
             <div class="logo">
-              <Link to='/'><img src={logo} alt="collegeLogo" /> </Link>
-              
+              <Link to="/">
+                <img src={logo} alt="collegeLogo" />
+              </Link>
             </div>
           </div>
           <div className="name-text">
             <p> Government Institution, RajanuKunte </p>
-            <p> सरकारी संस्थान, राजनुकुंते
-          </p>
+            <p> सरकारी संस्थान, राजनुकुंते</p>
             <p> ಸರ್ಕಾರಿ ಸಂಸ್ಥೆ, ರಾಜನಕುಂಟೆ </p>
           </div>
-          {isOpen ? '' : <IoMenu className="menu-icon" onClick={toggleNavbar} fontSize={50}/>}
-         
-          
+          {isOpen ? (
+            ""
+          ) : (
+            <IoMenu
+              className="menu-icon"
+              onClick={toggleNavbar}
+              fontSize={50}
+            />
+          )}
         </div>
 
-        <div className={`nav-container-2 ${isOpen ? 'active' : ''}`}>
-        <IoClose className="close-menu-icon" onClick={toggleNavbar} fontSize={50} />
-          <ul className='nav-links'>
+        <div className={`nav-container-2 ${isOpen ? "active" : ""}`}>
+          <IoClose
+            className="close-menu-icon"
+            onClick={toggleNavbar}
+            fontSize={50}
+          />
+          <ul className="nav-links">
             <li>
-              <Link to="/" onClick={toggleNavbar}> Home </Link>
-            </li>
-            <li>
-            <button ref={academicsSubmenuRef} class="dropbtn"  onClick={() => toggleSubmenu("academics")}>Academics <FaCaretDown fontSize={18}/></button>
-              <ul className={`dropdown-content ${openSubmenu === "academics" ? 'submenu' : ''}`}>
-                <li><Link to='/academic-overview' onClick={toggleNavbar}><FaBook /> Academic Overview</Link></li>
-                <li><Link to='/timetable' onClick={toggleNavbar}><FaRegCalendarDays /> Timetables</Link></li>
-                <li><Link to='/scholarship' onClick={toggleNavbar}><SiGooglescholar /> Scholarships</Link></li>
-              </ul>
-            </li>
-            <li>
-            <button ref={admissionSubmenuRef} class="dropbtn" onClick={() => toggleSubmenu("admission")}>Admission <FaCaretDown fontSize={18}/></button>
-              <ul className={`dropdown-content ${openSubmenu === "admission" ? 'submenu' : ''}`}>
-                <li><Link to='/admission-overview' onClick={toggleNavbar}><FaBook /> Admission Overview</Link></li>
-                <li><Link to='/fee' onClick={toggleNavbar}><FaMoneyBill /> Fee Structure</Link></li>
+              <Link to="/" onClick={toggleNavbar}>
                 
+                Home
+              </Link>
+            </li>
+            <li>
+              <button
+                ref={academicsSubmenuRef}
+                class="dropbtn"
+                onClick={() => toggleSubmenu("academics")}
+              >
+                Academics <FaCaretDown fontSize={18} />
+              </button>
+              <ul
+                className={`dropdown-content ${
+                  openSubmenu === "academics" ? "submenu" : ""
+                }`}
+              >
+                <li>
+                  <Link to="/academic-overview" onClick={toggleNavbar}>
+                    <FaBook /> Academic Overview
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/timetable" onClick={toggleNavbar}>
+                    <FaRegCalendarDays /> Timetables
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/scholarship" onClick={toggleNavbar}>
+                    <SiGooglescholar /> Scholarships
+                  </Link>
+                </li>
               </ul>
             </li>
             <li>
-            <button ref={departmentSubmenuRef} class="dropbtn" onClick={() => toggleSubmenu("department")}>Department <FaCaretDown fontSize={18}/></button>
-              <ul className={`dropdown-content ${openSubmenu === "department" ? 'submenu' : ''}`}>
-                <li><Link to='/pu-college' onClick={toggleNavbar}><BiSolidSchool /> Pu College</Link></li>
-                <li><Link to='/high-school' onClick={toggleNavbar}><FaSchool /> High School</Link></li>
-                <li><Link to='/primary-school' onClick={toggleNavbar}><FaSchoolFlag /> Primary School</Link></li>
+              <button
+                ref={admissionSubmenuRef}
+                class="dropbtn"
+                onClick={() => toggleSubmenu("admission")}
+              >
+                Admission <FaCaretDown fontSize={18} />
+              </button>
+              <ul
+                className={`dropdown-content ${
+                  openSubmenu === "admission" ? "submenu" : ""
+                }`}
+              >
+                <li>
+                  <Link to="/admission-overview" onClick={toggleNavbar}>
+                    <FaBook /> Admission Overview
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/fee" onClick={toggleNavbar}>
+                    <FaMoneyBill /> Fee Structure
+                  </Link>
+                </li>
               </ul>
             </li>
             <li>
-              <Link to="/facilities" onClick={toggleNavbar}> Facilities </Link>
-            </li>
-            <li>
-            <button ref={gallerySubmenuRef} class="dropbtn" onClick={() => toggleSubmenu("gallery")}>Gallery <FaCaretDown fontSize={18}/></button>
-              <ul className={`dropdown-content ${openSubmenu === "gallery" ? 'submenu' : ''}`}>
-                <li><Link to='/gallery-images' onClick={toggleNavbar}><FaImage /> School Images</Link></li>
-                <li><Link to='/gallery-videos' onClick={toggleNavbar}><FaVideo /> School Videos</Link></li>
+              <button
+                ref={departmentSubmenuRef}
+                class="dropbtn"
+                onClick={() => toggleSubmenu("department")}
+              >
+                Department <FaCaretDown fontSize={18} />
+              </button>
+              <ul
+                className={`dropdown-content ${
+                  openSubmenu === "department" ? "submenu" : ""
+                }`}
+              >
+                <li>
+                  <Link to="/pu-college" onClick={toggleNavbar}>
+                    <BiSolidSchool /> Pu College
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/high-school" onClick={toggleNavbar}>
+                    <FaSchool /> High School
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/primary-school" onClick={toggleNavbar}>
+                    <FaSchoolFlag /> Primary School
+                  </Link>
+                </li>
               </ul>
             </li>
             <li>
-            <button ref={aboutSubmenuRef} class="dropbtn"  onClick={() => toggleSubmenu("about")}>About <FaCaretDown fontSize={18}/></button>
-              <ul className={`dropdown-content ${openSubmenu === "about" ? 'submenu' : ''}`}>
-                <li><Link to='/development-committe' onClick={toggleNavbar}><FaUsers /> College Development commite</Link></li>
-                <li><Link to='/board-members' onClick={toggleNavbar}><ImProfile /> Board of Members</Link></li>
-                <li><Link to='/chairperson' onClick={toggleNavbar}><RiUserStarFill /> About Chairperson</Link></li>
-                <li><Link to='/principal' onClick={toggleNavbar}><FaUser /> Principal</Link></li>
+              <Link to="/facilities" onClick={toggleNavbar}>
+                
+                Facilities
+              </Link>
+            </li>
+            <li>
+              <button
+                ref={gallerySubmenuRef}
+                class="dropbtn"
+                onClick={() => toggleSubmenu("gallery")}
+              >
+                Gallery <FaCaretDown fontSize={18} />
+              </button>
+              <ul
+                className={`dropdown-content ${
+                  openSubmenu === "gallery" ? "submenu" : ""
+                }`}
+              >
+                <li>
+                  <Link to="/gallery-images" onClick={toggleNavbar}>
+                    <FaImage /> School Images
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/gallery-videos" onClick={toggleNavbar}>
+                    <FaVideo /> School Videos
+                  </Link>
+                </li>
               </ul>
             </li>
             <li>
-              <Link to="/contact" onClick={toggleNavbar}> Contact us </Link>
+              <button
+                ref={aboutSubmenuRef}
+                class="dropbtn"
+                onClick={() => toggleSubmenu("about")}
+              >
+                About <FaCaretDown fontSize={18} />
+              </button>
+              <ul
+                className={`dropdown-content ${
+                  openSubmenu === "about" ? "submenu" : ""
+                }`}
+              >
+                <li>
+                  <Link to="/development-committe" onClick={toggleNavbar}>
+                    <FaUsers /> College Development commite
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/board-members" onClick={toggleNavbar}>
+                    <ImProfile /> Board of Members
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/chairperson" onClick={toggleNavbar}>
+                    <RiUserStarFill /> About Chairperson
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/principal" onClick={toggleNavbar}>
+                    <FaUser /> Principal
+                  </Link>
+                </li>
+              </ul>
             </li>
             <li>
-              <Link to="/vision-mission" onClick={toggleNavbar}> Contact uss </Link>
+              <Link to="/contact" onClick={toggleNavbar}>
+                
+                Contact us
+              </Link>
             </li>
           </ul>
         </div>
