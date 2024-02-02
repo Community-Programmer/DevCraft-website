a6d4f4050c71ef9f8d94c448c92ec0edf1a023eb
Ankit Singh <147507772+Ankit27-09@users.noreply.github.com>
2024-02-03 01:31:35 +0530

    correct vision and mission page (#22)
    * adding vision and mission and correct info
    
    * correct vision mission page.
    
    ---------
    
    Co-authored-by: Sarthak Patel <76515568+Community-Programmer@users.noreply.github.com>

--- src/Pages/About/Vision and Mission/VisionMission.jsx
+++ src/Pages/About/Vision and Mission/VisionMission.jsx
@@ -5,20 +5,21 @@ const VisionMission = () => {
   return (
     <>
     <div class = "vision-container">
-    <h3 class = "vm heading"> VISION OF THE INSTITUTION </h3>
+
+    <h3 class = "vm-heading"> VISION OF THE INSTITUTION </h3>
       <hr/>
 
-    <div className = "vision">
+    <div class = "vision">
+
       <p> V1.  To provide quality education to students coming from all strata of the society. To shape the life of each student for a better future.</p> 
       <p>  V2.  Fostering a sense of community by promoting collaboration and involvement of students, parents, and educators in the educational journey.</p>
     </div>
 
-
-  
-    <h3 className = "vm heading"> MISSION OF THE INSTITUTION </h3>
+    <h3 class = "vm-heading"> MISSION OF THE INSTITUTION </h3>
       <hr/>
 
-    <div className = "vision"> 
+    <div class = "vision"> 
+
      <p> M1. To create technologically upgraded institution which caters to the educational standards of today's times. </p> 
      <p> M2. To create a strong and transformative technical educational environment in which fresh ideas, moral principles, research and excellence nurture.</p>
     </div>
