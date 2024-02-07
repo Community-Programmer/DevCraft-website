3105554bac2f5d31927e4dd099ecad5279d60ea4
SarthakRawat-1 <sarthakrawat525@gmail.com>
2024-02-07 16:33:24 +0530

    Vision

--- src/Pages/About/Vision and Mission/VisionMission.jsx
+++ src/Pages/About/Vision and Mission/VisionMission.jsx
@@ -11,8 +11,8 @@ const VisionMission = () => {
 
     <div class = "vision">
 
-      <p> V1.  To provide quality education to students coming from all strata of the society. To shape the life of each student for a better future.</p> 
-      <p>  V2.  Fostering a sense of community by promoting collaboration and involvement of students, parents, and educators in the educational journey.</p>
+      <p>To provide quality education to students coming from all strata of the society.</p><p>To shape the life of each student for a better future.</p>
+      <p>Fostering a sense of community by promoting collaboration and involvement of students, parents, and educators in the educational journey.</p>
     </div>
 
     <h3 class = "vm-heading"> MISSION OF THE INSTITUTION </h3>
@@ -20,8 +20,8 @@ const VisionMission = () => {
 
     <div class = "vision"> 
 
-     <p> M1. To create technologically upgraded institution which caters to the educational standards of today's times. </p> 
-     <p> M2. To create a strong and transformative technical educational environment in which fresh ideas, moral principles, research and excellence nurture.</p>
+     <p>To create technologically upgraded institution which caters to the educational standards of today's times. </p> 
+     <p>To create a strong and transformative technical educational environment in which fresh ideas, moral principles, research and excellence nurture.</p>
     </div>
     </div>
     </>
